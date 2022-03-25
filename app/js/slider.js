6628664557fcc0d2868c93a97ce86b1d54255b0b
kuznecsova.Lisa <kuznecsovalisa@gmail.com>
2022-03-25 12:59:37 +0300

    fix(html): layout

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,25 +1,9 @@
-$('.reviews__list').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+var swiper = new Swiper(".reviews__list" ,{
+  slidesPerView: 3,
+  spaceBetween: 50,
+  centeredSlides: true, 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
